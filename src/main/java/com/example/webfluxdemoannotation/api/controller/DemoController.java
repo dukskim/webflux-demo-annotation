d2cbec3eabdc0f148a75ace84690a8f2ca14f186
kdh <kdh>
2025-04-28 00:20:51 +0900

    * message 추가 - 다국어 (Messagesource 예제)

--- src/main/java/com/example/webfluxdemoannotation/api/controller/DemoController.java
+++ src/main/java/com/example/webfluxdemoannotation/api/controller/DemoController.java
@@ -5,18 +5,27 @@ import com.example.webfluxdemoannotation.api.dto.response.DemoDataResponse;
 import com.example.webfluxdemoannotation.api.service.DemoService;
 import com.example.webfluxdemoannotation.base.dto.SingleResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.MessageSource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
+import java.util.Locale;
+
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class DemoController {
 
     private final DemoService demoService;
 
+    private final MessageSource messageSource;
+
     @GetMapping("/api/v1/demo/data")
-    public Mono<ResponseEntity<SingleResponse<DemoDataResponse>>> demoData() {
+    public Mono<ResponseEntity<SingleResponse<DemoDataResponse>>> demoData(Locale locale) {
+        String msg = messageSource.getMessage("DEF_SYS", null, locale);
+        log.info("MSG:::::{}", msg);
         return demoService.findDemoData()
                 .map(data -> ResponseEntity.ok(new SingleResponse<>(data)));
     }
